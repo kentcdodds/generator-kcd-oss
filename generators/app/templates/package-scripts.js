abefbe2e277b78155c2b688c8546cae9c5127131
Kent C. Dodds <kent@doddsfamily.us>
2017-05-29 20:09:06 -0600

    feat: remove semantic-release from deps

--- generators/app/templates/package-scripts.js
+++ generators/app/templates/package-scripts.js
@@ -3,6 +3,8 @@ const npsUtils = require('nps-utils')
 const series = npsUtils.series
 const concurrent = npsUtils.concurrent
 const rimraf = npsUtils.rimraf
+const commonTags = npsUtils.commonTags
+const oneLine = commonTags.oneLine
 
 module.exports = {
   scripts: {
@@ -17,7 +19,10 @@ module.exports = {
       },
     },
     commit: {
-      description: 'This uses commitizen to help us generate well formatted commit messages',
+      description: oneLine`
+        This uses commitizen to help us
+        generate well formatted commit messages
+      `,
       script: 'git-cz',
     },
     test: {
@@ -26,18 +31,20 @@ module.exports = {
     },
     build: {
       description: 'delete the dist directory and run babel to build the files',
-      script: series(rimraf('dist'), 'babel --copy-files --out-dir dist --ignore *.test.js src'),
+      script: series(
+        rimraf('dist'),
+        'babel --copy-files --out-dir dist --ignore __tests__ src',
+      ),
     },
     lint: {
       description: 'lint the entire project',
       script: 'eslint .',
     },
-    release: {
-      description: 'We automate releases with semantic-release. This should only be run on travis',
-      script: series('semantic-release pre', 'npm publish', 'semantic-release post'),
-    },
     validate: {
-      description: 'This runs several scripts to make sure things look good before committing or on clean install',
+      description: oneLine`
+        This runs several scripts to make sure things look
+        good before committing or on clean install
+      `,
       script: concurrent.nps('lint', 'build', 'test'),
     },
   },
@@ -49,7 +56,6 @@ module.exports = {
 // this is not transpiled
 /*
   eslint
-  max-len: 0,
   comma-dangle: [
     2,
     {
